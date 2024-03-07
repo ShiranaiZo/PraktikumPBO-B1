f1cbcfc8143f05a17128ca91b7cbe6c1268fc6dd
ShiranaiZo <faishalbariqmaulana123@gmail.com>
2024-03-07 12:39:15 +0700

    tugas 3

--- Praktikum 3/Asersi2.java
+++ Praktikum 3/Asersi2.java
@@ -15,7 +15,7 @@ public class Asersi2 {
     public static void main(String[] args) {
         double jariJari = 0;
 
-        assert(jariJari != 0): "jari-jari tidak boleh nol!!!";
+        assert(jariJari > 0): "jari-jari tidak boleh nol!!!";
         Lingkaran l = new Lingkaran(jariJari);
         
         double kelilingLingkaran = l.hitungKeliling();
